b5a6db08e8a789b88f75ec9d743538ccef1e0945
farrenhi <114633763+farrenhi@users.noreply.github.com>
2023-07-27 22:19:29 +0800

    Add files via upload

---
+++ week4/app.py
@@ -0,0 +1,69 @@
+from flask import Flask, request, render_template, redirect, url_for, session
+
+app = Flask(
+    __name__,
+    static_folder="static",
+    static_url_path="/static",
+)
+
+app.secret_key = "secret" # session would need this!
+
+@app.route("/") # , methods=["GET"]
+def index():
+    return render_template("index.html")
+
+# Verification Endpoint
+@app.route("/signin", methods=["POST"])
+def signin():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == '' or password == '':
+        message_info = "Please enter username and password"
+        return redirect(url_for("error", message=message_info))
+        
+    if username == 'test' and password == 'test':
+        session["SIGNED-IN"] = True  # Set the "SIGNED-IN" state to True in the session.
+        session["username"] = username
+        return redirect(url_for("member"))
+    else:
+        message_info = "Username or password is not correct"
+        return redirect(url_for("error", message=message_info))
+
+@app.route("/member")
+def member():
+    if session.get("SIGNED-IN"):
+        # User is signed in, render the success page.
+        return render_template("member.html")
+    else:
+        # User is not signed in, redirect to the login page or show an error message.
+        return redirect(url_for("index"))  # Redirect to the login page if not signed in.
+
+@app.route("/error")
+def error():
+    data_received = request.args.get("message")
+    return render_template("error.html", data=data_received)
+
+# @app.route("/calculate", methods=["POST"])
+# def calculate():
+#     # Get method: this does not show up in the URL
+#     #maxNumber = request.args.get("max", "")
+    
+#     # Post method: you need to use request! This method is good for privacy
+#     maxNumber = request.form["max"]
+#     maxNumber = int(maxNumber)
+#     result = 0
+#     for n in range(1, maxNumber + 1):
+#         result += n
+#     # "Result:" + str(result)
+#     return render_template("result.html", data=result)
+
+@app.route('/signout', methods=["GET"])
+def signout():
+    session["SIGNED-IN"] = False
+    # remove the username from the session if it's there
+    username = session["username"]
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# run server
+app.run(port=3000)
